776ffbc79dfaf2e51f23f42f4b4beb7f3e939505
Alvaro Daza Quiroga <Alvaro Daza@jalasoft.local>
2017-11-22 19:21:43 -0400

    solving push

--- steps/workspace_steps.py
+++ steps/workspace_steps.py
@@ -13,14 +13,10 @@ def step_impl(context, method, end_point):
 @when(u'I save the workspaces id as <workspaces_id>')
 def step_impl(context):
     context.workspace_id = (context.response.json())['id']
-    print(context.response.status_code)
-    print(context.workspace_id)
 
 
 @when(u'I send {method} request to {end_point}')
 def step_impl(context, method, end_point):
     end_point = end_point.replace('<workspaces_id>', str(context.workspace_id))
     project_data = json.loads(context.text.replace(context.project_name, str(context.project_id)))
-    print(end_point)
-    print(project_data)
     context.response = context.request_api.execute_request(method, end_point, data=project_data)
